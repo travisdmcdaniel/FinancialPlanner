af3f3b7a3398299c121b08892ea4594c465b2c21
Travis McDaniel <travis.d.mcdaniel@gmail.com>
2017-08-22 11:54:16 -0400

    Add project files.

---
+++ FinancialPlanner/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinancialPlanner.MvcApplication" Language="C#" %>
---
+++ FinancialPlanner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinancialPlanner.Startup))]
+namespace FinancialPlanner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
